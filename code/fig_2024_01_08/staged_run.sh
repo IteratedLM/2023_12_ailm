e89096cd0994310ae3f39953f713a5801fca3c1c
Con <conor.houghton@bristol.ac.uk>
2024-04-23 13:21:39 +0100

    m

--- code/fig_2024_01_08/staged_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for batchC in {1..20} 
-do
-    sbatch runFig.sh $batchC
-    sleep 60
-done
-
-echo "All iterations complete."
--- code/fig_2024_01_09/staged_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for trial0 in {1..25} 
-do
-    sbatch runFig.sh $trial0
-    sleep 60
-done
-
-echo "All iterations complete."
